efda3a31b39010fe5f970e5e1f4db8f04d2ddd5e
irensvm <irens.vm@gmail.com>
2020-09-15 20:13:03 +0200

    initial commit to heroku

---
+++ public/precache-manifest.c34b97095aac548013a39288b86c5695.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5271420579e8189cc07176eae2de50d7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d0bca5810aabf4e0e375",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "fa94183b55b1ddbe97d3",
+    "url": "/static/css/main.5ad564e2.chunk.css"
+  },
+  {
+    "revision": "d0bca5810aabf4e0e375",
+    "url": "/static/js/2.9591a2eb.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.9591a2eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa94183b55b1ddbe97d3",
+    "url": "/static/js/main.f45a6606.chunk.js"
+  },
+  {
+    "revision": "965a57521bd79bd78fdc",
+    "url": "/static/js/runtime-main.42b52a06.js"
+  }
+]);
